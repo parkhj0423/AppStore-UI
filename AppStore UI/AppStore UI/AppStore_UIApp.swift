2b949dad898170249641c8703381fc33646cadf9
Parkhyunwoo <parkhj0423@naver.com>
2023-03-08 22:15:16 +0900

    [parkhj0423] Initial Commit

---
+++ AppStore UI/AppStore UI/AppStore_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStore_UIApp.swift
+//  AppStore UI
+//
+//  Created by 박현우 on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct AppStore_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
